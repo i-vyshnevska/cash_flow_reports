4a503a82eefaf4d35b400bfc2ca9a369bb9a5496
Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
2021-08-03 16:24:36 +0300

    fixup! fixup! [ADD][10.0] cash_flow_report

--- cash_flow_report/report/account_cash_book.py
+++ cash_flow_report/report/account_cash_book.py
@@ -11,9 +11,40 @@ class ReportCashBook(models.AbstractModel):
     _description = 'Cash Book Report'
 
 
-    def _get_account_move_entry(self, accounts, form_data, pass_date):
+    def _get_account_move_entry(self, accounts, init_balance, form_data, pass_date):
         cr = self.env.cr
         move_line = self.env['account.move.line']
+        move_lines = {x: [] for x in accounts.ids}
+
+        # Prepare initial sql query and Get the initial move lines
+        if init_balance:
+            init_tables, init_where_clause, init_where_params = move_line.with_context(
+                date_from=self.env.context.get('date_from'), date_to=False,
+                initial_bal=True)._query_get()
+            init_wheres = [""]
+            if init_where_clause.strip():
+                init_wheres.append(init_where_clause.strip())
+            init_filters = " AND ".join(init_wheres)
+            filters = init_filters.replace('account_move_line__move_id',
+                                           'm').replace('account_move_line',
+                                                        'l')
+            sql = ("""SELECT 0 AS lid, l.account_id AS account_id, '' AS ldate, '' AS lcode, 0.0 AS amount_currency, '' AS lref, 'Initial Balance' AS lname, COALESCE(SUM(l.debit),0.0) AS debit, COALESCE(SUM(l.credit),0.0) AS credit, COALESCE(SUM(l.debit),0) - COALESCE(SUM(l.credit), 0) as balance, '' AS lpartner_id,\
+                    '' AS move_name, '' AS mmove_id, '' AS currency_code,\
+                    NULL AS currency_id,\
+                    '' AS invoice_id, '' AS invoice_type, '' AS invoice_number,\
+                    '' AS partner_name\
+                    FROM account_move_line l\
+                    LEFT JOIN account_move m ON (l.move_id=m.id)\
+                    LEFT JOIN res_currency c ON (l.currency_id=c.id)\
+                    LEFT JOIN res_partner p ON (l.partner_id=p.id)\
+                    JOIN account_journal j ON (l.journal_id=j.id)\
+                    WHERE l.account_id IN %s""" + filters + ' GROUP BY l.account_id')
+            params = (tuple(accounts.ids),) + tuple(init_where_params)
+            cr.execute(sql, params)
+            for row in cr.dictfetchall():
+                move_lines[row.pop('account_id')].append(row)
+
+
         tables, where_clause, where_params = move_line._query_get()
         wheres = [""]
         if where_clause.strip():
